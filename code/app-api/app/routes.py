6d4766201faa3da148db397ac3d19dbfa09f52e5
Arabinda Dora <arabinda.dora@klarna.com>
2024-04-27 15:19:02 +0200

    implement file upload and analysis APIs

--- code/app-api/app/routes.py
+++ code/app-api/app/routes.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 import logging
 
-from . import graphql, init
+from . import graphql, init, ai
 
 logger = logging.getLogger(__name__)
 
@@ -9,8 +9,24 @@ logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
+
 @app.on_event("startup")
 async def reinit():
     init.init()
 
+
 app.include_router(graphql.get_app(), prefix="/api")
+
+
+@app.post("/upload")
+def upload(file: UploadFile = File(...)):
+    with open(f"/tmp/{file.filename}", "wb") as f:
+        f.write(file.file.read())
+    return {"status": "ok"}
+
+
+@app.get("/analysis")
+async def analysis(file: str):
+    with open(f"/tmp/{file}", "r") as f:
+        code = f.read()
+        return ai.analyze(code)
